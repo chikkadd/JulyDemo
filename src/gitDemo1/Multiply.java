9d1276cc5b215e5d11765471c479cb5319e4acde
Chikke <chikkadd20@gmail.com>
2022-07-19 08:41:54 +0530

    my first commit code

---
+++ src/gitDemo1/Multiply.java
@@ -0,0 +1,12 @@
+package gitDemo1;
+
+public class Multiply
+{
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println("multiplication of 2 numbers = "+c);
+	}
+
+}
